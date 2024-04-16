8d84c65a361bca3442ea4e8f9218e5f99bf887ea
SaTriX43 <santiagofg43@gmail.com>
2024-04-16 11:45:54 -0500

    aniadimos funcionalidad de verificar errores y agregamos la validacion falta corregir deteccion de errores

--- validador-tl.js
+++ validador-tl.js
@@ -10,40 +10,58 @@ export function validarNumeroTelefonico(input,revisar,limpiar,mostrar) {
         $mostrar = d.querySelector(mostrar);
 
 
-        // 3.-crear funcion para errores 
-        function detectarErrores(numero){
-           
+        // creanos una funcion de mostrarMensaje() 
+        function mostrarMensaje(mensaje,esValido) {
+            let $mensaje = d.createElement('div');
+                $mensaje.textContent = mensaje;
+
+                if(esValido) {
+                    $mensaje.classList.add('correct');
+                }else {
+                    $mensaje.classList.add('incorrect')
+                }
+
+                $mostrar.appendChild($mensaje)
+        }
+        
+        // --------------------------------------------------------------------------------- 
+        // creamos funcion que detectar si el numero es valido
+        function validacion(numero) {
+            // declaramos variable numero 
+            let numeros = numero.replace(/\D/g, "");
+
             // si el valor es igual a vacio entonces 
             if(numero === '') {
-                 // mostramos en un alert
+                // mostramos en un alert
                 alert('Please provide a phone number')
                 return false;
             }
 
             // si el valor no es un numero del 1 al 9 y no es - entonces 
-            if(!/^[-0-9]+$/.test(numero)){
-                $mostrar.innerHTML += `
-                <p>Invalid US number: ${numero}</p>`
+            else if(!/^[-0-9\s()+]+$/.test(numero)){
+                // llamamos funcion mostrarMensaje 
+                mostrarMensaje(`Invalid US number: ${numero}`,false)
+                return false
             }
 
+            // si la longitud de numeros es igual a 10 entonces 
+            else if(numeros.length === 10 || (numeros.length === 11 && numeros.startsWith('1'))) {
+                // llamamos funcion mostrarMensaje 
+                mostrarMensaje(`Valid US number: ${numero}`,true)
 
+            }else{//si no
+                // llamamos function mostrarMensaje 
+                mostrarMensaje(`Invalid US number: ${numero}`,false)
+            }
         }
-
-        
-        // creamos funcion que detectar si el numero es valido
-        function validarNumero(numero) {
-            let numeros = numero.replace(/\D/g, "");
-
-            return numeros.length === 10;
-        }
-
 
         // creamos listener en el document 
         d.addEventListener('click',e=> {
 
             // si el evento es igual a $revisar entonces 
             if(e.target === $revisar) {
-                detectarErrores($input.value)
+                validacion($input.value)
+                $input.value = ""
             }
 
             // si el evento es igual a $limpiar entonces 
